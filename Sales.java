2a72a0a778f02ce95ebec97532869df57b6e7294
Carrissa Smith <smithcarrissa1@gmail.com>
2022-11-29 08:04:10 -0800

    Sales class

---
+++ Sales.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class Sales
+{
+        private int car_id, cust_id;
+        private Date start_date;
+        private Date present_date;
+        private Date date_sold;
+        public ArrayList<Integer> carsSold= new ArrayList<Integer>(); 
+
+    
+    public void Sales(int car_id, int cust_id, Date start_date, Date present_date)
+    {
+        this.car_id = car_id;
+        this.cust_id = cust_id;
+        this.start_date = start_date;
+        this.present_date = present_date;
+        
+    }
+
+    public void updateCarsSold(int car_id, int cust_id, Date date_sold)
+    {
+        carsSold.add(car_id);
+    }
+
+    public productionAnalysisReport(Date start_date, Date present_date)
+    {
+        int sales_sum;
+        for()
+        if(Customer_Progress.customer_progress(cust_id)== "transaction approved" ){
+            sal
+        }
+    }
+        
+}
